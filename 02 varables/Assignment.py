c23aa7f6fea654ba6fa71567a8474f3e378f0921
kaustav-mondal-cg <kaustav.mondal.cg@gmail.com>
2026-08-22 16:00:17 +0530

    push 2nd lec to repo

---
+++ 02 varables/Assignment.py
@@ -0,0 +1,43 @@
+#Qustion-24
+
+name="Abhay"
+age=19
+city="Mumbai"
+print(name,age,city)
+
+#Question-25
+
+student_name = "Abhay"
+student_roll=20
+student_branch="Btech cse"
+print(student_name,student_roll,student_branch)
+
+#Question-26
+
+marks=50
+marks=49
+print(marks)
+
+#Question-27
+
+name,age,city = "Abhay",19,"mumbai"
+print(name,age,city)
+
+#Question-28
+
+x=y=z=0
+print(x,y,z)
+
+#Question-29
+
+student1 = "Rahul"
+student_name = "Rahul"
+student_branch= "B.Tech"
+print(student1,student_name,student_branch)
+
+#Question-30
+
+student_name="Abhay"
+student_age=19
+student_age=20
+print(student_name,student_age)
---
+++ 02 varables/intro.py
@@ -0,0 +1,9 @@
+#Student-Info
+name = "Rahul"
+name = "Ram"
+age = 23
+marks = 456/500
+print(name,age,marks)
+
+name, age, marks = "Rounak", 18, 345
+
