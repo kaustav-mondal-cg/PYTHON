e5256648d4f46e331347239d27b2938f16d40a91
kaustav-mondal-cg <kaustav.mondal.cg@gmail.com>
2026-08-24 13:02:43 +0530

    adding 3rd lec to repo

---
+++ 03_DataTypes/intro.py
@@ -0,0 +1,20 @@
+name = "Ram" #String
+age = 23 #Integer
+marks = 475.5 #Float
+is_student = True #Boolean
+result = None #Nonetype
+print (''' Hello Everyone "hi" python's class ''')
+print (type(age)) # <class 'int'>
+
+
+age = float(age)
+print(age,type(age))
+
+name = str(name)          #can only be str
+print(name,type(name))
+
+marks = bool(marks)
+print(marks,type(marks))
+
+is_student = None
+print(is_student,type(is_student))
