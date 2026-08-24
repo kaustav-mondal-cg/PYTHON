d869f6bc024a64de92d69a7b5f3d755325e217cb
kaustav-mondal-cg <kaustav.mondal.cg@gmail.com>
2026-08-24 17:48:23 +0530

    Adding assignment of 03 in repo

---
+++ 03_DataTypes/assignment.py
@@ -0,0 +1,85 @@
+# 24. Student Information
+
+student_name = "Rahul"
+student_age = 20
+student_height = 5.8
+is_student = True
+student_result = None
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(is_student))
+print(type(student_result))
+
+
+# 25. Integer, Float and String
+
+a = 50
+b = 50.0
+c = "50"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# 26. True vs "True"
+
+a = True
+b = "True"
+
+print(type(a))
+print(type(b))
+
+
+# 27. None vs "None"
+
+a = None
+b = "None"
+
+print(type(a))
+print(type(b))
+
+
+# 28. Changing the Type of a Variable
+
+value = 100
+print(type(value))
+
+value = "Hello"
+print(type(value))
+
+
+# 29. Product Information
+
+product_name = "Laptop"
+product_quantity = 5
+product_price = 49999.99
+product_available = True
+product_discount = None
+
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(product_available))
+print(type(product_discount))
+
+
+# 30. Different Data Types
+
+a = 10
+b = 10.0
+c = "10"
+d = True
+e = "True"
+f = None
+g = "None"
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
