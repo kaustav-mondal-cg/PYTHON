90eafe95885ffd745bbb8897d56417216068b8d8
kaustav-mondal-cg <kaustav.mondal.cg@gmail.com>
2026-08-29 09:59:35 +0530

    Moving String to repo

---
+++ 05 String_Operatores/nextclass.py
@@ -0,0 +1,39 @@
+word = "Python"
+
+word = word[:3] + "ho" + word[4:]
+
+print(word)
+
+name = "JoHn"
+
+print(name.upper())
+print(name.lower())
+
+text = "pythON PROGRAMMING"
+
+print(text.capitalize())
+
+text = "python programming language"
+
+print(text.title())
+
+message = "Hello Python"
+
+print("Python" in message)
+print("Java" in message)
+print("Java" not in message)
+
+text = "Hello Python"
+
+print(text.find("Python"))
+print(text.find("Java"))
+print(text.index("Python"))
+# print(text.index("Java")) #error : substring not found.[Value Error]
+
+Text = "banana"
+
+print(Text.count("a"))
+
+
+a = "Hello"
+print(text.index("l"))
