30f2e51d96ee446d21d7ca31a45ff5523558d81e
kaustav-mondal-cg <kaustav.mondal.cg@gmail.com>
2026-08-29 09:59:35 +0530

    Adding String again in repo

---
+++ 05 String_Operatores/intro.py
@@ -0,0 +1,17 @@
+a="python"
+b="python"
+print(a==b)
+print(b[0])
+print(len(b))
+
+# P Y T H O N 
+# 0 1 2 3 4 5
+#-6-5-4-3-2-1
+print(len("Kaustav "))
+print(a[2:6])
+print(a[::])
+
+c=b
+print(len(c))
+
+print("Hello")
